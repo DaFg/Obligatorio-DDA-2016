58ba572870e7abaf313406a04b18cbab29e7ac48
David <piranha_2003@hotmail.com>
2016-09-19 19:15:29 -0300

    Commit del directorio del proyecto

---
+++ Codigo/MonopolyDDA/Monopolio/src/main/java/Monopoly/DDA/Monopolio/App.java
@@ -0,0 +1,13 @@
+package Monopoly.DDA.Monopolio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

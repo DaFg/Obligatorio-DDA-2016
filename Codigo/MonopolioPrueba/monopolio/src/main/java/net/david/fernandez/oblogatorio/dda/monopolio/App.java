fe99a2353d8a5f8fd4e3cdbcc891e49b875c9106
David <piranha_2003@hotmail.com>
2016-10-21 19:21:29 -0300

    se elimino el proyecto anteriro.

---
+++ Codigo/MonopolioPrueba/monopolio/src/main/java/net/david/fernandez/oblogatorio/dda/monopolio/App.java
@@ -0,0 +1,13 @@
+package net.david.fernandez.oblogatorio.dda.monopolio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

fb897cba927760d53bd2b5320e5485cf947f00e3
Paablo15 <pisp06@gmail.com>
2016-11-22 19:03:20 -0300

    Alo

--- Codigo/MonopolioPrueba/monopolio/server/src/main/java/net/david/fernandez/oblogatorio/dda/server/dominio/Monopoly.java
+++ Codigo/MonopolioPrueba/monopolio/server/src/main/java/net/david/fernandez/oblogatorio/dda/server/dominio/Monopoly.java
@@ -13,7 +13,7 @@ public class Monopoly {
 
 		board[0] = new Go();
 		board[1] = new Estate("Avenida Mediterranea", 60, 30, 2, 50, 50, 10, 30, 90, 160, 250);
-		board[2] = new Casilla("Cofre comunidad");
+		board[2] = new Comunidad("Cofre comunidad");
 		board[3] = new Estate("Avenida Baltico", 60, 30, 4, 50, 50, 20, 60, 180, 320, 450);
 		board[4] = new Casilla("Jail");
 

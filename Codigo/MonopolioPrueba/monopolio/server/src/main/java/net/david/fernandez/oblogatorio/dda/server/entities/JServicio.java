2fa810c0c5d432e30ddfe2ae824539fc618b50a8
David <piranha_2003@hotmail.com>
2016-11-25 18:41:56 -0300

    Creacion Jservicio1.

---
+++ Codigo/MonopolioPrueba/monopolio/server/src/main/java/net/david/fernandez/oblogatorio/dda/server/entities/JServicio.java
@@ -0,0 +1,42 @@
+package net.david.fernandez.oblogatorio.dda.server.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import net.david.fernandez.oblogatorio.dda.server.dominio.Login;
+
+public class JServicio {
+
+	public LoginController LoginCorrecto(String nombre, char[] contrasenia) {
+		// --Desde aqui JPA--//
+
+		System.out.println("Comienza JPA");
+		LoginController login = new LoginController();
+		EntityManagerFactory emf;
+
+		System.out.println("Creo EMF");
+		emf = Persistence.createEntityManagerFactory("jpaDS");
+
+		System.out.println("Creo EM");
+		EntityManager em = (EntityManager) emf.createEntityManager();
+
+		System.out.println("Comienzo transaccion");
+		em.getTransaction().begin();
+
+		// Consulat comienzo//
+		String sql = "select * from login where nimbre = +";
+		Query query = em.createNativeQuery(sql);
+	
+		//Estoy aqui estaba con las consultas a la bd.//
+		//List<Long> resultado = query.getResultList();
+		// Consulat fin//
+
+		System.out.println("Finalizo Transaccion");
+		em.getTransaction().commit();
+
+		return login;
+	}
+
+}
